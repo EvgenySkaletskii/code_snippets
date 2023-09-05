e2d7f7f08a2623c0b6078b5517c67344506b1ce7
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2023-09-05 19:47:45 +0400

    Add simple factory method pattern

---
+++ patterns/factory_method_v1.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class User
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+class Admin < User
+  
+end
+
+class UserFactory
+  def self.create(type)
+    case type
+    when 'admin'
+      Admin.new
+    when 'member'
+      Member.new
+    else
+      Guest.new
+    end
+  end
+end
+
+class Endpoint
+  def foo(params)
+    user = UserFactory.create(params[:user_type])
+    # some operations with user
+  end
+end
